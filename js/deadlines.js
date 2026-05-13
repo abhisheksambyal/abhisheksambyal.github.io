63f53acb37ae670823e148043a213e8be7c45e54
Abhishek Singh Sambyal <abhishek.sambyal@gmail.com>
2026-05-14 00:21:31 +0300

    feat: prioritize abstract/registration deadlines over full submission deadlines

--- js/deadlines.js
+++ js/deadlines.js
@@ -15,20 +15,20 @@
   var FALLBACK = {
     last_updated: "May 13, 2026",
     conferences: [
-      { name: "WACV", year: 2027, link: "https://wacv.thecvf.com/", deadline: "2026-06-25", place: "Buena Vista, FL", date: "January 5 - January 9, 2027" },
-      { name: "AAAI", year: 2027, link: "https://aaai.org/", deadline: "2026-08-01", place: "TBA", date: "Feb 2027 (est.)", estimated: true },
-      { name: "ICLR", year: 2027, link: "https://iclr.cc/", deadline: "2026-09-24", place: "TBA", date: "Apr 2027 (est.)", estimated: true },
-      { name: "AISTATS", year: 2027, link: "https://aistats.org/", deadline: "2026-10-02", place: "TBA", date: "Apr-May 2027 (est.)", estimated: true },
-      { name: "CVPR", year: 2027, link: "https://cvpr.thecvf.com/", deadline: "2026-11-13", place: "TBA", date: "Jun 2027 (est.)", estimated: true },
+      { name: "WACV", year: 2027, link: "https://wacv.thecvf.com/", deadline: "2026-06-18", place: "Buena Vista, FL", date: "January 5 - January 9, 2027" },
+      { name: "AAAI", year: 2027, link: "https://aaai.org/", deadline: "2026-07-25", place: "TBA", date: "Feb 2027 (est.)", estimated: true },
+      { name: "ICLR", year: 2027, link: "https://iclr.cc/", deadline: "2026-09-19", place: "TBA", date: "Apr 2027 (est.)", estimated: true },
+      { name: "AISTATS", year: 2027, link: "https://aistats.org/", deadline: "2026-09-25", place: "TBA", date: "Apr-May 2027 (est.)", estimated: true },
+      { name: "CVPR", year: 2027, link: "https://cvpr.thecvf.com/", deadline: "2026-11-07", place: "TBA", date: "Jun 2027 (est.)", estimated: true },
       { name: "ISBI", year: 2027, link: "https://biomedicalimaging.org/", deadline: "2026-11-14", place: "TBA", date: "Apr 2027 (est.)", estimated: true },
       { name: "MIDL", year: 2027, link: "https://midl.io/", deadline: "2026-12-10", place: "TBA", date: "Jul 2027 (est.)", estimated: true },
       { name: "IPMI", year: 2027, link: "https://ipmi-conference.org/", deadline: "2026-12-13", place: "TBA", date: "Jun 2027 (est.)", estimated: true },
-      { name: "ICML", year: 2027, link: "https://icml.cc/", deadline: "2027-01-28", place: "TBA", date: "Jul 2027 (est.)", estimated: true },
-      { name: "ECCV", year: 2027, link: "https://ecva.net/", deadline: "2027-02-15", place: "TBA", date: "Sep 2027 (est.)", estimated: true },
+      { name: "ICML", year: 2027, link: "https://icml.cc/", deadline: "2027-01-23", place: "TBA", date: "Jul 2027 (est.)", estimated: true },
+      { name: "MICCAI", year: 2027, link: "https://miccai.org/", deadline: "2027-02-12", place: "TBA", date: "Sep–Oct 2027 (est.)", estimated: true },
       { name: "UAI", year: 2027, link: "https://auai.org/", deadline: "2027-02-25", place: "TBA", date: "Jul 2027 (est.)", estimated: true },
-      { name: "MICCAI", year: 2027, link: "https://miccai.org/", deadline: "2027-02-26", place: "TBA", date: "Sep–Oct 2027 (est.)", estimated: true },
-      { name: "ICCV", year: 2027, link: "https://thecvf.com/", deadline: "2027-03-08", place: "TBA", date: "Oct 2027 (est.)", estimated: true },
-      { name: "NeurIPS", year: 2027, link: "https://neurips.cc/", deadline: "2027-05-06", place: "TBA", date: "Dec 2027 (est.)", estimated: true },
+      { name: "ECCV", year: 2027, link: "https://ecva.net/", deadline: "2027-02-26", place: "TBA", date: "Sep 2027 (est.)", estimated: true },
+      { name: "ICCV", year: 2027, link: "https://thecvf.com/", deadline: "2027-03-04", place: "TBA", date: "Oct 2027 (est.)", estimated: true },
+      { name: "NeurIPS", year: 2027, link: "https://neurips.cc/", deadline: "2027-05-04", place: "TBA", date: "Dec 2027 (est.)", estimated: true },
       { name: "BMVC", year: 2027, link: "https://bmva.org/", deadline: "2027-05-10", place: "TBA", date: "Nov 2027 (est.)", estimated: true }
     ]
   };
